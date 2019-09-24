ef55a8d9e16a4c28388e50467559dc9de05487e4
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2019-09-24 02:18:10 +0100

    shallow renderer first test

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,22 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import ShallowRenderer from 'react-test-renderer/shallow';
 
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+
+    const renderer = new ShallowRenderer();
+    renderer.render(<App/>);
+    const result = renderer.getRenderOutput();
+
+    expect(result.type).toBe('div');
+// expect(result.props.children).toEqual([
+//   <span className="heading">Title</span>,
+//   <Subcomponent foo="bar" />
+// ]);
 });
