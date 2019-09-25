f7518a7acfd05d8682d74d5c4efe7ac64ed4ea94
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2019-09-25 02:17:02 +0100

    ppl list

---
+++ src/Person..js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Person = (props) => (
+    <div>
+        <div>{props.firstName}</div>
+        <div>{props.secondName}</div>
+    </div>
+);
+
+export default Person;
